93023a2ab296044585b2821b2a2f25d602d2a429
Jagannadh <bharat.verma@algoworks.com>
2019-11-27 18:54:20 +0530

    creating the searchbar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, Icon, Input, Button } from 'antd';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
@@ -10,14 +11,37 @@ class SearchBar extends Component {
         this.state = { term: '' }
     }
 
-    onInputChange(event) {
-        this.setState({ term: event.target.value });
+    onInputChange = e => {
+        this.setState({ term: e.target.value });
     }
 
-    onFormSubmit(event) {
-        event.preventDefault();
+    onFormSubmit = e => {
+        e.preventDefault();
 
         this.props.fetchWeather(this.state.term);
         this.setState({ term: '' });
     }
+
+    render() {
+        return (
+            <Form layout="inline" onSubmit={this.onFormSubmit}>
+                <Form.Item>
+                    <Input
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        placeholder="Type any city to get Weather Forecast"
+                        value={this.state.term}
+                        onChange={this.onInputChange}
+                    />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Search
+          </Button>
+                </Form.Item>
+            </Form>
+        )
+    }
 }
+
+export default connect(null, actions)(SearchBar);
