47988655dbb4d12d4a1f832f78882ec8e37eea6e
Jagannadh <bharat.verma@algoworks.com>
2019-11-28 22:26:07 +0530

    adding basic styling and adding search function

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import WeatherList from './waether_list';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <h1> Weather App! </h1>
+                <SearchBar />
+                <WeatherList />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import ReduxPromise from 'redux-promise';
+ 
 import './index.css';
 import App from './components/app';
 
@@ -11,7 +12,7 @@ import reducers from './reducers';
 
 import * as serviceWorker from './serviceWorker';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(ReduxPromise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
